b311af29f906add9faf35e8d9c83f9ca3bea9015
thienthn <tutybukuny@gmail.com>
2024-01-07 22:34:12 +0700

    feat: logger v2

---
+++ l2/l_test.go
@@ -0,0 +1,61 @@
+package l2
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	"github.com/thnthien/great-deku/l2/telegram"
+)
+
+func TestNew(t *testing.T) {
+	t.Setenv("LOG_LEVEL", "trace")
+
+	ll := Builder{
+		File: &lumberjack.Logger{
+			Filename:   "logs/logs.log",
+			MaxSize:    1,
+			MaxAge:     7,
+			MaxBackups: 10,
+		},
+	}.Build()
+	//ll = NewWithSentry(&sentry.Configuration{
+	//	DSN:   "https://6c823523782944c597fcc102c8b6ae4e@o390151.ingest.sentry.io/5231166",
+	//	Trace: struct{ Disabled bool }{Disabled: false},
+	//})
+	defer ll.Sync()
+	a := map[string]interface{}{
+		"testdebug": 1,
+	}
+	ll.Trace("test trace", Any("test trace", a))
+	ll.Debug("test debug", Any("test debug", a))
+	ll.Info("test info", Any("test debug", a))
+	ll.Warn("test warn")
+	//ll.Panic("fatal")
+	ll.Error("test err")
+
+	ctx := context.WithValue(context.Background(), RequestIDCtxKey, "test_id")
+	ll.DebugCtx(ctx, "test request_id", String("message", "test"))
+	ll.InfoCtx(ctx, "test request_id", String("message", "test"))
+	ll.WarnCtx(ctx, "test request_id", String("message", "test"))
+	ll.ErrorCtx(ctx, "test request_id", String("message", "test"))
+
+	logger := ll.With(String("specific_value", "testing"))
+	logger.DebugCtx(ctx, "test request_id", String("message", "test"))
+	logger.InfoCtx(ctx, "test request_id", String("message", "test"))
+	logger.WarnCtx(ctx, "test request_id", String("message", "test"))
+	logger.ErrorCtx(ctx, "test request_id", String("message", "test"))
+}
+
+func Test_TelegramLog(t *testing.T) {
+	ll := Builder{
+		Telegram: &telegram.Config{
+			Token:  "",
+			ChatID: 0,
+		},
+	}.Build()
+	ll.Info("test info")
+	ll.Error("test error", Error(errors.New("this error is for testing")))
+}
---
+++ l2/logger.go
@@ -0,0 +1,119 @@
+package l2
+
+import (
+	"context"
+	"github.com/thnthien/great-deku/l2/sentry"
+	"log"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	RequestIDCtxKey = "requestid"
+)
+
+type LoggerOption func(logger *Logger)
+
+func WithRequestIDCtxKey(requestIDCtxKey string) LoggerOption {
+	return func(logger *Logger) {
+		logger.requestIDCtxKey = requestIDCtxKey
+	}
+}
+
+func WithLogLevel(level string) LoggerOption {
+	return func(logger *Logger) {
+		err := logger.logLevel.UnmarshalText([]byte(level))
+		if err != nil {
+			log.Panicf("invalid log level: %s", err)
+		}
+	}
+}
+
+func WithZapOptions(opts []zap.Option) LoggerOption {
+	return func(logger *Logger) {
+		logger.zapOpts = opts
+	}
+}
+
+// Logger wraps zap.Logger
+type Logger struct {
+	*zap.Logger
+
+	zapOpts         []zap.Option
+	sentryCfg       *sentry.Configuration
+	logLevel        Level
+	requestIDCtxKey string
+}
+
+func (l *Logger) With(fields ...zap.Field) *Logger {
+	logger := l.Logger.With(fields...)
+	return &Logger{
+		Logger:          logger,
+		zapOpts:         l.zapOpts,
+		sentryCfg:       l.sentryCfg,
+		logLevel:        l.logLevel,
+		requestIDCtxKey: l.requestIDCtxKey,
+	}
+}
+
+func (l *Logger) Trace(msg string, fields ...zap.Field) {
+	l.Log(TraceLevel, msg, fields...)
+}
+
+func (l *Logger) TraceCtx(ctx context.Context, msg string, fields ...zap.Field) {
+	l.LogCtx(ctx, TraceLevel, msg, fields...)
+}
+
+func (l *Logger) DebugCtx(ctx context.Context, msg string, fields ...zap.Field) {
+	l.LogCtx(ctx, zapcore.DebugLevel, msg, fields...)
+}
+
+func (l *Logger) InfoCtx(ctx context.Context, msg string, fields ...zap.Field) {
+	l.LogCtx(ctx, zapcore.InfoLevel, msg, fields...)
+}
+
+func (l *Logger) WarnCtx(ctx context.Context, msg string, fields ...zap.Field) {
+	l.LogCtx(ctx, zapcore.WarnLevel, msg, fields...)
+}
+
+func (l *Logger) ErrorCtx(ctx context.Context, msg string, fields ...zap.Field) {
+	l.LogCtx(ctx, zapcore.ErrorLevel, msg, fields...)
+}
+
+func (l *Logger) LogCtx(ctx context.Context, level zapcore.Level, msg string, fields ...zap.Field) {
+	rid := l.getRequestID(ctx)
+	if rid != "" {
+		fields = append(fields, String("request_id", rid))
+	}
+
+	var log func(string, ...zap.Field)
+
+	switch level {
+	case TraceLevel:
+		log = l.Trace
+	case zapcore.DebugLevel:
+		log = l.Debug
+	case zapcore.InfoLevel:
+		log = l.Info
+	case zapcore.WarnLevel:
+		log = l.Warn
+	case zapcore.ErrorLevel:
+		log = l.Error
+	case zapcore.DPanicLevel:
+		log = l.DPanic
+	case zapcore.PanicLevel:
+		log = l.Panic
+	case zapcore.FatalLevel:
+		log = l.Fatal
+	case zapcore.InvalidLevel:
+		log = l.Panic
+	}
+
+	log(msg, fields...)
+}
+
+func (l *Logger) getRequestID(ctx context.Context) string {
+	rid, _ := ctx.Value(l.requestIDCtxKey).(string)
+	return rid
+}
